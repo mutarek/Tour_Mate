0680434b1fc7c3e1cc1a244954ff8657923b1041
kazishihan <kazishihan@gmail.com>
2019-04-05 00:32:07 +0600

    current location weather

--- app/src/main/java/com/kazishihan/tourmate/WeatherActivity.java
+++ app/src/main/java/com/kazishihan/tourmate/WeatherActivity.java
@@ -1,5 +1,11 @@
 package com.kazishihan.tourmate;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,6 +14,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.kazishihan.tourmate.Weither.WeatherResult;
 import com.squareup.picasso.Picasso;
 
@@ -17,13 +29,20 @@ import retrofit2.Response;
 
 public class WeatherActivity extends AppCompatActivity {
 
-    private TextView currentWeatherDiscription,currentWeathertemp,currentWeatherWind,currentWeatherHumidity;
+    private TextView currentWeatherDiscription, currentWeathertemp, currentWeatherWind, currentWeatherHumidity;
     private ImageView currentWeatherIcon;
 
     private RecyclerView recyclerView;
     private WeitherAdapter weatherAdapter;
     private WeatherResult weatherResult;
     private WeatherResult currentWeatherResult;
+
+    private double lat = 0;
+    private double lon = 0;
+    private String units = "metric";
+    String url;
+
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,19 +56,54 @@ public class WeatherActivity extends AppCompatActivity {
         currentWeatherHumidity = findViewById(R.id.humidityCurrentWeitherTvId);
 
         weatherResult = new WeatherResult();
-        recyclerView=findViewById(R.id.weatherRecyclerViewId);
-        getWeatherUpdate();
+        recyclerView = findViewById(R.id.weatherRecyclerViewId);
+            //getLocationPermission();
+/////////////permission///////////
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getMyLocation();
 
-
-
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
 
 
     }
 
+    private void getMyLocation() {
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
+        locationTask.addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if(task.isSuccessful())
+                {
+                    Location location = task.getResult();
+                    url =String.format("forecast?lat=%f&lon=%f&units=%s&appid=%s",location.getLatitude(),location.getLongitude(),units,getResources().getString(R.string.appid));
+                   // Toast.makeText(WeatherActivity.this, String.valueOf(location.getLatitude()), Toast.LENGTH_SHORT).show();
+                    getWeatherUpdate();
+                }
+
+            }
+        });
+
+    }
+
+
     private void getWeatherUpdate() {
+
         IOpenWeatherMap iOpenWeatherMap= RetrofitClass.getRetrofitInstance().create(IOpenWeatherMap.class);
 
-        Call<WeatherResult> weatherResultCall= iOpenWeatherMap.getWeatherData("forecast?lat=23.7533312&lon=90.3769738&units=metric&appid=a0e0d52b2dbb8228d3f19466bb398fd0");
+//        String url =String.format("forecast?lat=%f&lon=%f&units=%s&appid=%s",lat,lon,units,getResources().getString(R.string.appid));
+       //"forecast?lat=23.7533312&lon=90.3769738&units=metric&appid=a0e0d52b2dbb8228d3f19466bb398fd0"
+
+
+        Call<WeatherResult> weatherResultCall= iOpenWeatherMap.getWeatherData(url);
 
         weatherResultCall.enqueue(new Callback<WeatherResult>() {
             @Override
@@ -69,7 +123,7 @@ public class WeatherActivity extends AppCompatActivity {
                             .append(".png").toString()).into(currentWeatherIcon);
                     currentWeathertemp.setText("Temp: "+weatherResult.getList().get(0).getMain().getTemp()+" °C");
                     currentWeatherWind.setText("Wind :"+weatherResult.getList().get(0).getWind().getSpeed()+" km/h");
-
+                   // Toast.makeText(WeatherActivity.this, ""+weatherResult.getCity().getCoord().getLat(), Toast.LENGTH_SHORT).show();
                 }
             }
 
